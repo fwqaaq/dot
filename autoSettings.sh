1a5942ca0b5d2ea2017b0851692c739ee6904575
fwqaaq <fwqaaq@gmail.com>
2023-06-07 23:20:42 +0800

    update: add color function

--- autoSettings.sh
+++ autoSettings.sh
@@ -13,37 +13,49 @@ Nvm="https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh"
 
 choses=("install" "exit")
 
+green() {
+        echo -e "\e[32m $1 \e[0m"
+}
+
+yellow() {
+        echo -e "\e[33m $1 \e[0m"
+}
+
+red() {
+        echo -e "\e[34m $1 \e[0m"
+}
+
 installPlugins() {
         local pluginsPath_sug="${Custom}/plugins/zsh-autosuggestions"
         local pluginsPath_sea="${Custom}/plugins/zsh-history-substring-search"
         local PluginsPath_hig="${Custom}/plugins/zsh-syntax-highlighting"
         local ThemePath_pow="${Custom}/themes/powerlevel10k"
         if [ ! -d "$pluginsPath_sug" ]; then
-                echo -e "\e[32mInstalling $ZshAutoSuggestions ....\e[0m"
-                git clone --depth=1 $ZshAutoSuggestions $pluginsPath_sug
+                green "Installing $ZshAutoSuggestions ...."
+                git clone --depth=1 "$ZshAutoSuggestions" "$pluginsPath_sug"
         fi
         if [ ! -d "$pluginsPath_sea" ]; then
-                echo -e "\e[32mInstalling $ZshHistorySubStringSearch ....\e[0m"
-                git clone --depth=1 $ZshHistorySubStringSearch $pluginsPath_sea
+                green "Installing $ZshHistorySubStringSearch ...."
+                git clone --depth=1 "$ZshHistorySubStringSearch" "$pluginsPath_sea"
         fi
         if [ ! -d "$PluginsPath_hig" ]; then
-                echo -e "\e[32mInstalling $ZshSyntaxHighLighting ....\e[0m"
-                git clone --depth=1 $ZshSyntaxHighLighting $PluginsPath_hig
+                green "Installing $ZshSyntaxHighLighting ...."
+                git clone --depth=1 "$ZshSyntaxHighLighting" "$PluginsPath_hig"
         fi
         if [ ! -d "$ThemePath_pow" ]; then
-                echo -e "\e[32mInstalling $Powerlevel10k ....\e[0m"
-                git clone --depth=1 $Powerlevel10k $ThemePath_pow
+                green "Installing $Powerlevel10k ...."
+                git clone --depth=1 "$Powerlevel10k" "$ThemePath_pow"
         fi
 }
 
 installNvm() {
         if command -v node >/dev/null 2>&1; then
-                echo "\e[32mHi, you already have node, we will jump! You can chose by youself...\e[0m"
+                green "Hi, you already have node, we will jump! You can chose by youself.."
         fi
         select chose in "${choses[@]}"; do
                 case $chose in
                 "install")
-                        echo -e "\e[32mstart automatic intall for you\e[0m"
+                        green "start automatic intall for you"
                         (curl -o- $Nvm | bash) &
                         wait $!
                         source ~/.zshrc
@@ -55,11 +67,11 @@ installNvm() {
                         break
                         ;;
                 "exit")
-                        echo -e "\e[34mYou need to install by yourself\e[0m"
+                        yellow "You need to install by yourself."
                         exit 1
                         ;;
                 *)
-                        echo -e "\e[31mInvalid entry\e[0m"
+                        red "Invalid entry"
                         ;;
                 esac
         done
@@ -67,43 +79,43 @@ installNvm() {
 
 installTmux() {
         if ! command -v tmux >/dev/null 2>&1; then
-                echo -e "\e[32mHi, you don't yet have tmux, we will exit!...\e[0m"
+                green "Hi, you don't yet have tmux, we will exit!..."
                 exit 1
         fi
 
-        echo -e "\e[31m Let's start to install .tmux.conf and its plugins.\e[0m"
+        green "Let's start to install .tmux.conf and its plugins."
         curl -o ~/.tmux.conf "${Root}.tmux.conf"
         git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-        echo -e "\e[31mDone. But you must open tmux, and use 'Ctrl-a' + 'Shift-i' to install others plugins.\e[0m"
+        yellow "Done. But you must open tmux, and use 'Ctrl-a' + 'Shift-i' to install others plugins."
 }
 
 if ! command -v git >/dev/null 2>&1; then
-        echo -e "\e[31mPlease install git at first.\e[0m"
+        red "Please install git at first."
         exit 1
 fi
 
 if [[ -n "$SHELL" && "${SHELL##*/}" != "zsh" ]]; then
         if command -v zsh >/dev/null 2>&1; then
                 if chsh -s "$(which zsh)"; then
-                        echo -e "\e[31mZsh is now the default shell. Please log out and log back in to apply the changes.\e[0m"
+                        yellow "Zsh is now the default shell. Please log out and log back in to apply the changes."
                 else
-                        echo -e "\e[31m You need to install chsh command at first. Or You can set zsh by manually.\e[0m"
+                        yellow "You need to install chsh command at first. Or You can set zsh by manually."
                         exit 1
                 fi
         else
-                echo -e "\e[31mZsh is not installed. You must intall zsh at first.\e[0m"
+                red "Zsh is not installed. You must intall zsh at first."
                 exit 1
         fi
 fi
 
 if [ ! -n "$ZSH" ] || [ ! -f "$ZSH/oh-my-zsh.sh" ]; then
-        echo -e "\e[32m              You need to install oh-my-zsh, you can chose 1 or 2\e[0m"
-        echo -e "\e[32m              1, it can automatic intall for you\e[0m"
-        echo -e "\e[32m              2, then by youself by manually, shell will be exited\e[0m"
+        green "              You need to install oh-my-zsh, you can chose 1 or 2"
+        green "              1, it can automatic intall for you"
+        green "              2, then by youself by manually, shell will be exitem"
         select chose in "${choses[@]}"; do
                 case $chose in
                 "install")
-                        echo -e "\e[32mstart automatic intall for you\e[0m"
+                        green "start automatic intall for you"
                         (
                                 sh -c "$(curl -fsSL $OhMyZSH)"
                         ) &
@@ -111,56 +123,60 @@ if [ ! -n "$ZSH" ] || [ ! -f "$ZSH/oh-my-zsh.sh" ]; then
                         break
                         ;;
                 "exit")
-                        echo -e "\e[34mYou need to install by yourself\e[0m"
+                        yellow "You need to install by yourself"
                         exit 1
                         ;;
                 *)
-                        echo -e "\e[31mInvalid entry\e[0m"
+                        red "Invalid entry"
                         break
                         ;;
                 esac
         done
 fi
 
-read -p "\e[32mDo you want to install p10k config? Please intput (y/n)\e[0m" answer
+green "Do you want to install p10k config? Please intput (y/n)"
+read answer
+
 if [ "$answer" = "y" ]; then
         if installPlugins; then
                 curl -o ~/.zshrc "${Root}.zshrc"
                 curl -o ~/.p10k.zsh "${Root}.p10k.zsh"
                 echo -e "\e[42;30mComplete plugin download\e[0m"
         else
-                echo "\e[31mExcution failed! Please view the question!\e[0m"
+                red "Excution failed! Please view the question!"
         fi
 else
-        echo -e "\e[32mContinue....\e[0m"
+        green "Continue...."
 fi
 
 # Install nvm for node
-
-read -p "\e[32mDo you want to install nvm to manage node config? Please intput (y/n)\e[0m" answer
+green "Do you want to install nvm to manage node config? Please intput (y/n)"
+read answer
 
 if [ "$answer" = "y" ]; then
         if installNvm; then
                 echo -e "\e[42;30mComplete nvm and pnpm download\e[0m"
-                echo -e "Please exec \e[42msource ~/.zshrc\e[0m"
+                yellow "Please exec \e[41msource ~/.zshrc\e[0m"
         else
-                echo "\e[31mExcution failed! Please view the question!\e[0m"
+                red "Excution failed! Please view the question!"
         fi
 else
-        echo -e "\e[32mContinue.....\e[0m"
+        green "Continue....."
 fi
 
 # Install tmux
-
-read -p "\e[32mDo you want to install tmux config? Please intput (y/n)\e[0m" answer
+green "Do you want to install tmux config? Please intput (y/n)"
+read answer
 
 if [ "$answer" = "y" ]; then
-        echo -e "\e[32mStart....\e[0m"
+        green "Start...."
         if installTmux; then
-                echo -e "\e[32mComplete tmux download.\e[0m"
+                green "Complete tmux download."
         else
-                echo "\e[31mExcution failed! Please view the question!\e[0m"
+                red "Excution failed! Please view the question!"
         fi
 else
-        echo -e "Countine...."
+        green "Countine...."
 fi
+
+red "Please log out and login in again."
